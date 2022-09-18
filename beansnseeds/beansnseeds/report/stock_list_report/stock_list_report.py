1563540d3f1903ac709c637229661c9a92e06ade
sammish <sammish.thundiyil@gmail.com>
2022-09-18 10:49:07 -0500

    updated report

--- beansnseeds/beansnseeds/report/stock_list_report/stock_list_report.py
+++ beansnseeds/beansnseeds/report/stock_list_report/stock_list_report.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2022, sammish and contributors
 # For license information, please see license.txt
+# sammish.thundiyil@gmail.com
 
 from __future__ import unicode_literals
 import frappe
